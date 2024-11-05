573a4e9d3cbf25071024c53eb2d78c479430bac1
Deepak P <worksofdeep@gmail.com>
2024-11-05 16:58:29 +0530

    Minor improvements and changes

---
+++ packages/angular-workspace/projects/api-lib/src/lib/stencil-generated/components.ts
@@ -0,0 +1,30 @@
+/* tslint:disable */
+/* auto-generated angular directive proxies */
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, NgZone } from '@angular/core';
+
+import { ProxyCmp } from './angular-component-lib/utils';
+
+import { Components } from 'api-test';
+
+
+@ProxyCmp({
+})
+@Component({
+  selector: 'api-test',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  // eslint-disable-next-line @angular-eslint/no-inputs-metadata-property
+  inputs: [],
+})
+export class ApiTest {
+  protected el: HTMLElement;
+  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
+    c.detach();
+    this.el = r.nativeElement;
+  }
+}
+
+
+export declare interface ApiTest extends Components.ApiTest {}
+
+
